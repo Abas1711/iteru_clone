59caeb0969652c62727fc78b03161ce78e373630
Ahmed <ahmedabbas20we@gmail.com>
2025-02-19 15:00:40 +0200

    Fix mongoose issue and update dependencies

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
+const mongoose = require('mongoose');
 const express = require("express");
 const connectDB = require("./config/db");
+
 
 
 const app = express();
@@ -9,6 +11,7 @@ app.use(express.json());
 
 connectDB();
 
+
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/museums", require("./routes/museumRoutes"));
 app.use("/api/images", require("./routes/imageRoutes"));
@@ -16,6 +19,10 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+console.log("MONGO_URI:", process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Database connected successfully"))
+  .catch(err => console.log("DB Connection Error:", err));
 
 
 
