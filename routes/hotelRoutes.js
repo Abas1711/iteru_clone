4079df8f6d8638fd0c245e987b755ccc7102361c
Ahmed <ahmedabbas20we@gmail.com>
2025-06-11 12:35:02 +0300

    aded

--- routes/hotelRoutes.js
+++ routes/hotelRoutes.js
@@ -4,16 +4,16 @@ const path = require("path");
 const router = express.Router();
 
 // GET: List all hotels
-router.get("/", (req, res) => {
-  const filePath = path.join(__dirname, "../data/hotels.json");
-  fs.readFile(filePath, "utf8", (err, data) => {
-    if (err) {
-      return res.status(500).json({ message: "Error reading hotel data" });
-    }
-    const hotels = JSON.parse(data);
-    res.json(hotels);
-  });
-});
+// router.get("/", (req, res) => {
+//   const filePath = path.join(__dirname, "../data/hotels.json");
+//   fs.readFile(filePath, "utf8", (err, data) => {
+//     if (err) {
+//       return res.status(500).json({ message: "Error reading hotel data" });
+//     }
+//     const hotels = JSON.parse(data);
+//     res.json(hotels);
+//   });
+// });
 router.get("/", (req, res) => {
   const city = req.query.city?.toLowerCase();
   const filePath = path.join(__dirname, "../data/hotels.json");
