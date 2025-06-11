60db3eddee745cddd9a436e738c41cae9f00706e
Ahmed <ahmedabbas20we@gmail.com>
2025-06-11 12:32:08 +0300

    aded

--- routes/hotelRoutes.js
+++ routes/hotelRoutes.js
@@ -14,5 +14,19 @@ router.get("/", (req, res) => {
     res.json(hotels);
   });
 });
+router.get("/", (req, res) => {
+  const city = req.query.city?.toLowerCase();
+  const filePath = path.join(__dirname, "../data/hotels.json");
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) return res.status(500).json({ message: "Error reading hotel data" });
+
+    let hotels = JSON.parse(data);
+    if (city) {
+      hotels = hotels.filter(h => h.city.toLowerCase() === city);
+    }
+    res.json(hotels);
+  });
+});
+
 
 module.exports = router;
